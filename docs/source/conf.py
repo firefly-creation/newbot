932a2012e0c2a02bf6a5fabaa0865fcb12212271
cv-robot <1074311905@qq.com>
2024-06-13 20:11:59 +0800

    first commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,13 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))  # 将项目根目录添加到模块查找路径中去
+
+
+
+
 project = 'newbot'
 copyright = '2024, luowei'
 author = 'luowei'
@@ -14,7 +21,10 @@ release = '1.3.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +34,10 @@ language = 'zh_CN'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
 html_static_path = ['_static']
+
+
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
